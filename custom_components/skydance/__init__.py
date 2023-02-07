a9a81e917383cd1ba689bf126009cd87ac4788e7
Tomas Bedrich <ja@tbedrich.cz>
2023-02-07 14:37:16 +0100

    Upgrade to async_forward_entry_setups,
    See: https://developers.home-assistant.io/blog/2022/07/08/config_entry_forwards/

--- custom_components/skydance/__init__.py
+++ custom_components/skydance/__init__.py
@@ -17,7 +17,7 @@ async def async_setup(hass, config):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
